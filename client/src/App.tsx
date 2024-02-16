958577d7a2db0ee0ea5bccf9d7fbd122af739444
kumarsatwik <kumarsatwik6@gmail.com>
2024-02-16 13:09:42 +0530

    added shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <Button variant="link">Button</Button>
+    </>
+  );
+}
+
+export default App;
